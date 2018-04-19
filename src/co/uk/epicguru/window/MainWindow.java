8b1d81b3e735ad006086559c1ba1905ce3ff422f
James Billy <james@epicguru.co.uk>
2018-04-19 20:30:58 +0200

    Adding more graphics for buttons and such.

--- src/co/uk/epicguru/window/MainWindow.java
+++ src/co/uk/epicguru/window/MainWindow.java
@@ -5,6 +5,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 
 import co.uk.epicguru.main.assets.Assets;
+import java.awt.Color;
+import javax.swing.BoxLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.SpringLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Window.Type;
 
 public class MainWindow {
 
@@ -35,10 +45,39 @@ public class MainWindow {
 	 */
 	private void initialize() {
 		frmSkillwarzLauncher = new JFrame();
+		frmSkillwarzLauncher.setBackground(Color.LIGHT_GRAY);
+		frmSkillwarzLauncher.getContentPane().setBackground(Color.DARK_GRAY);
+		SpringLayout springLayout = new SpringLayout();
+		frmSkillwarzLauncher.getContentPane().setLayout(springLayout);
+		
+		JLabel titleLogo = new JLabel("");
+		springLayout.putConstraint(SpringLayout.NORTH, titleLogo, 10, SpringLayout.NORTH, frmSkillwarzLauncher.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, titleLogo, 10, SpringLayout.WEST, frmSkillwarzLauncher.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, titleLogo, 88, SpringLayout.NORTH, frmSkillwarzLauncher.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, titleLogo, -10, SpringLayout.EAST, frmSkillwarzLauncher.getContentPane());
+		titleLogo.setIcon(new ImageIcon(MainWindow.class.getResource("/co/uk/epicguru/main/assets/Logo.png")));
+		frmSkillwarzLauncher.getContentPane().add(titleLogo);
+		
+		JButton playButton = new JButton("PLAY");
+		playButton.setBackground(new Color(152, 251, 152));
+		playButton.setFont(new Font("Tahoma", Font.BOLD, 40));
+		springLayout.putConstraint(SpringLayout.NORTH, playButton, -67, SpringLayout.SOUTH, frmSkillwarzLauncher.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, playButton, 10, SpringLayout.WEST, frmSkillwarzLauncher.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, playButton, -10, SpringLayout.SOUTH, frmSkillwarzLauncher.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, playButton, 160, SpringLayout.WEST, frmSkillwarzLauncher.getContentPane());
+		frmSkillwarzLauncher.getContentPane().add(playButton);
+		
+		JLabel stateLabel = new JLabel("Ready to play!");
+		springLayout.putConstraint(SpringLayout.EAST, stateLabel, 0, SpringLayout.EAST, titleLogo);
+		stateLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		springLayout.putConstraint(SpringLayout.NORTH, stateLabel, -67, SpringLayout.SOUTH, frmSkillwarzLauncher.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, stateLabel, -36, SpringLayout.SOUTH, frmSkillwarzLauncher.getContentPane());
+		stateLabel.setForeground(Color.WHITE);
+		springLayout.putConstraint(SpringLayout.WEST, stateLabel, 6, SpringLayout.EAST, playButton);
+		frmSkillwarzLauncher.getContentPane().add(stateLabel);
 		frmSkillwarzLauncher.setIconImage(Assets.loadImage("Icon.png"));
 		frmSkillwarzLauncher.setTitle("Skillwarz Launcher");
-		frmSkillwarzLauncher.setBounds(100, 100, 450, 300);
+		frmSkillwarzLauncher.setBounds(100, 100, 729, 487);
 		frmSkillwarzLauncher.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
 }
