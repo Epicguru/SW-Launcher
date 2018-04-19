404cb996eb79a5ffded69bd2427c1bac1100bc3d
James Billy <james@epicguru.co.uk>
2018-04-19 20:09:09 +0200

    Added improved asset loading code.

--- src/co/uk/epicguru/main/assets/Assets.java
+++ src/co/uk/epicguru/main/assets/Assets.java
@@ -2,15 +2,33 @@ package co.uk.epicguru.main.assets;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.imageio.ImageIO;
 
+import co.uk.epicguru.main.Debug;
+
 public final class Assets {
 
+	public static HashMap<String, BufferedImage> loadedImages = new HashMap<String, BufferedImage>();
+	
 	public static BufferedImage loadImage(String internalPath){
 		
+		// Loads an image from the internal co.uk.epicguru.main.assets classpath.
+		if(internalPath == null){
+			Debug.log("Null loadImage input, null output!");
+			return null;
+		}
+		
+		if(loadedImages.containsKey(internalPath)){
+			return loadedImages.get(internalPath);
+		}
+		
 		try {
-			return ImageIO.read(Assets.class.getResource(internalPath));
+			// Load internally and cache for faster reading later.
+			BufferedImage img = ImageIO.read(Assets.class.getResource(internalPath));
+			loadedImages.put(internalPath, img);
+			return img;
 		} catch (IOException e) {			
 			e.printStackTrace();
 			return null;
@@ -18,4 +36,18 @@ public final class Assets {
 		
 	}
 	
+	public static void unloadImage(String internalPath){
+		
+		// Unloads any cached version of an image.
+		if(internalPath == null){
+			return;
+		}
+		
+		if(!loadedImages.containsKey(internalPath)){
+			return;
+		}
+		
+		// Remove the image from the map.
+		loadedImages.remove(internalPath);
+	}
 }
